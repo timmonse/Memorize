c2d59fae84c1aa03e492802bbb79a26666e65ef7
Evan Timmons <evangeorgetimmons@gmail.com>
2020-05-22 14:30:56 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Evan Timmons on 5/22/20.
+//  Copyright © 2020 Evan Timmons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
