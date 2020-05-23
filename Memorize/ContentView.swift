cb8c0057e099e2a65a1ecfa8470ca993a4f7fb5e
Evan Timmons <evangeorgetimmons@gmail.com>
2020-05-23 16:16:20 -0500

    Lecture 1 functional code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack() {
+            ForEach(0..<4) { inedex in
+                CardView(isFaceUp: false);
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack(){
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke()
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
     }
 }
 
