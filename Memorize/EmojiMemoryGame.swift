774d49f18cd51b7a2a2f40050067fcf9af468fb2
Evan Timmons <evangeorgetimmons@gmail.com>
2020-05-25 17:22:14 -0500

    Completed code for lecture 2

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Evan Timmons on 5/25/20.
+//  Copyright © 2020 Evan Timmons. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+             return emojis[pairIndex]
+        }
+    }
+        
+    // MARK: - Access to the model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card : MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
+
+struct EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
